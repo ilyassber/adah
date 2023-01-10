062927b9871050abd2d0ad37bc42c12047f62cf3
ilyassber <ilyass.berchida@gmail.com>
2023-01-10 23:17:42 +0100

    fix multiple jumps animation

--- components/menu/ShurikenMenu.tsx
+++ components/menu/ShurikenMenu.tsx
@@ -11,7 +11,9 @@ type ShurikenMenuItemProps = {
     stopItem: string;
     animate: boolean;
     animationDirection: string;
+    speedUp?: boolean;
     onClick: () => void;
+    onAnimationComplete?: () => void;
 };
 
 type ShurikenMenuProps = {
@@ -30,7 +32,7 @@ const ShurikenMenuItem: React.FC<ShurikenMenuItemProps> = (props) => {
             `${props.animationDirection == "up" ? 2 / (Math.abs(props.index - 5) + 1) : 2 / (Math.abs(props.index - 3) + 1)}rem`
         ],
         fontWeight: 800,
-        color: ((props.index == 5 && props.animationDirection == "up") || (props.index == 3 && props.animationDirection == "down") ? "#D19E18" : "#9197A0"),
+        color: (props.speedUp ? "#9197A0" : ((props.index == 5 && props.animationDirection == "up") || (props.index == 3 && props.animationDirection == "down") ? "#D19E18" : "#9197A0")),
         opacity: [
             `${100 - (30 * (Math.abs(props.index - 4)))}%`,
             `${props.animationDirection == "up" ? 100 - (30 * Math.abs(props.index - 5)) : 100 - (30 * Math.abs(props.index - 3))}%`
@@ -38,21 +40,34 @@ const ShurikenMenuItem: React.FC<ShurikenMenuItemProps> = (props) => {
     } : {
         y: "0%",
         x: `${10 * (Math.abs(props.index - 4))}px`,
-        fontSize: `${2 / (Math.abs(props.index - 3) + 1)}rem`,
+        fontSize: `${2 / (Math.abs(props.index - 4) + 1)}rem`,
         fontWeight: 800,
         color: (props.index == 4 ? "#D19E18" : "#9197A0"),
         opacity: `${100 - (30 * (Math.abs(props.index - 4)))}%`,
     });
-    const [duration, setDuration] = React.useState<any>(0.5);
+    const [duration, setDuration] = React.useState<any>(props.animate ? (props.speedUp ? 0.2 : 0.4) : 0);
     const [value, setValue] = React.useState<string>(props.animate ? props.startItem : props.stopItem);
 
     React.useEffect(() => {
-        console.log(props.startItem, props.stopItem);
+        console.log("Render");
     }, []);
 
     React.useEffect(() => {
+        //console.log("speed up: ", props.speedUp);
+    }, [props.speedUp]);
+
+    React.useEffect(() => {
+        //console.log("duration: ", duration);
+    }, [duration]);
+
+    React.useEffect(() => {
+        //console.log("animate: ", props.animate);
+    }, [props.animate]);
+
+    React.useEffect(() => {
+        console.log(props.startItem, props.stopItem, duration, props.speedUp, props.animate);
         setValue(props.animate ? props.startItem : props.stopItem);
-        setDuration(0.5);
+        setDuration(props.animate ? (props.speedUp ? 0.2 : 0.4) : 0);
         setAnimation(props.animate ? {
             y: ["0%", (props.animationDirection == "up" ? "-100%" : "100%")],
             x: [
@@ -64,7 +79,7 @@ const ShurikenMenuItem: React.FC<ShurikenMenuItemProps> = (props) => {
                 `${props.animationDirection == "up" ? 2 / (Math.abs(props.index - 5) + 1) : 2 / (Math.abs(props.index - 3) + 1)}rem`
             ],
             fontWeight: 800,
-            color: ((props.index == 5 && props.animationDirection == "up") || (props.index == 3 && props.animationDirection == "down") ? "#D19E18" : "#9197A0"),
+            color: (props.speedUp ? "#9197A0" : ((props.index == 5 && props.animationDirection == "up") || (props.index == 3 && props.animationDirection == "down") ? "#D19E18" : "#9197A0")),
             opacity: [
                 `${100 - (30 * (Math.abs(props.index - 4)))}%`,
                 `${props.animationDirection == "up" ? 100 - (30 * Math.abs(props.index - 5)) : 100 - (30 * Math.abs(props.index - 3))}%`
@@ -72,7 +87,7 @@ const ShurikenMenuItem: React.FC<ShurikenMenuItemProps> = (props) => {
         } : {
             y: "0%",
             x: `${10 * (Math.abs(props.index - 4))}px`,
-            fontSize: `${2 / (Math.abs(props.index - 3) + 1)}rem`,
+            fontSize: `${2 / (Math.abs(props.index - 4) + 1)}rem`,
             fontWeight: 800,
             color: (props.index == 4 ? "#D19E18" : "#9197A0"),
             opacity: `${100 - (30 * (Math.abs(props.index - 4)))}%`,
@@ -85,16 +100,22 @@ const ShurikenMenuItem: React.FC<ShurikenMenuItemProps> = (props) => {
             transition={{ duration: duration }}
             animate={animation}
             onAnimationComplete={() => {
-                setValue(props.stopItem);
-                setDuration(0);
-                setAnimation({
-                    y: "0%",
-                    x: `${10 * (Math.abs(props.index - 4))}px`,
-                    fontSize: `${2 / (Math.abs(props.index - 4) + 1)}rem`,
-                    fontWeight: 800,
-                    color: (props.index == 4 ? "#D19E18" : "#9197A0"),
-                    opacity: `${100 - (30 * (Math.abs(props.index - 4)))}%`,
-                });
+                if (duration == 0) {
+                    if (props.onAnimationComplete) {
+                        props.onAnimationComplete();
+                    }
+                } else {
+                    setDuration(0);
+                    setValue(props.stopItem);
+                    setAnimation({
+                        y: "0%",
+                        x: `${10 * (Math.abs(props.index - 4))}px`,
+                        fontSize: `${2 / (Math.abs(props.index - 4) + 1)}rem`,
+                        fontWeight: 800,
+                        color: (props.speedUp ? "#9197A0" : (props.index == 4 ? "#D19E18" : "#9197A0")),
+                        opacity: `${100 - (30 * (Math.abs(props.index - 4)))}%`,
+                    });
+                }
             }}
         >
             <p
@@ -113,7 +134,11 @@ const ShurikenMenu: React.FC<ShurikenMenuProps> = (props) => {
 
     const menuRef = React.useRef<HTMLDivElement>(null);
 
-    const [selectedIndex, setSelectedIndex] = React.useState<number>(2);
+    const [selectedIndex, setSelectedIndex] = React.useState<number>(1);
+    const [prevIndex, setPrevIndex] = React.useState<number>(1);
+    const [currentIndex, setCurrentIndex] = React.useState<number>(1);
+    const [animate, setAnimate] = React.useState<boolean>(false);
+    const [speedUp, setSpeedUp] = React.useState<boolean>(false);
     const [animationDirection, setAnimationDirection] = React.useState<string>("up");
     const [places, setPlaces] = React.useState<any[] | null>(null);
     const items = [
@@ -152,27 +177,63 @@ const ShurikenMenu: React.FC<ShurikenMenuProps> = (props) => {
         if (menuRef && menuRef.current) {
             menuRef.current.addEventListener("wheel", (event: any) => {
                 event.preventDefault();
-                console.log(event);
             });
         }
     }, []);
 
     React.useEffect(() => {
+        if (currentIndex < selectedIndex) {
+            setPrevIndex(currentIndex);
+            setCurrentIndex(currentIndex + 1);
+        } else if (currentIndex > selectedIndex) {
+            setPrevIndex(currentIndex);
+            setCurrentIndex(currentIndex - 1);
+        }
+    }, [selectedIndex]);
+
+    React.useEffect(() => {
         let newPlacesList = [];
+        if (currentIndex == selectedIndex) {
+            setSpeedUp(false);
+        } else {
+            setSpeedUp(true);
+        }
+        if (prevIndex < currentIndex) {
+            setAnimate(true);
+            setAnimationDirection("up");
+        } else if (prevIndex > currentIndex) {
+            setAnimate(true);
+            setAnimationDirection("down");
+        }
         for (let i = 0; i < 7; i++) {
-            if (selectedIndex - 4 + i >= 0 && selectedIndex - 4 + i < 5) {
-                newPlacesList.push(items[selectedIndex - 4 + i]);
+            if (currentIndex - 4 + i >= 0 && currentIndex - 4 + i < 5) {
+                newPlacesList.push(items[currentIndex - 4 + i]);
             } else {
                 newPlacesList.push(null);
             }
         }
-        console.log(newPlacesList);
         setPlaces(newPlacesList);
-    }, [selectedIndex]);
+    }, [currentIndex]);
 
     React.useEffect(() => {
-        console.log(places);
+        console.log("places are changed");
     }, [places]);
+
+    React.useEffect(() => {
+        console.log(speedUp);
+    }, [speedUp]);
+
+    const onClick = (id: number) => {
+        setSelectedIndex(id);
+    };
+
+    const onAnimationComplete = () => {
+        if (currentIndex < selectedIndex) {
+            setCurrentIndex(currentIndex + 1);
+        } else if (currentIndex > selectedIndex) {
+            setCurrentIndex(currentIndex - 1);
+        }
+    };
 
     let content = (
         <div className={props.className}>
@@ -187,18 +248,15 @@ const ShurikenMenu: React.FC<ShurikenMenuProps> = (props) => {
                                     className=""
                                     startItem={animationDirection == "up" ? (places[index - 1] ? places[index - 1].name : "") : (places[index + 1] ? places[index + 1].name : "")}
                                     stopItem={place ? place.name : ""}
-                                    animate={true}
+                                    animate={animate}
                                     animationDirection={animationDirection}
+                                    speedUp={speedUp}
                                     onClick={() => {
                                         if (place) {
-                                            if (place.id > selectedIndex) {
-                                                setAnimationDirection("up");
-                                            } else {
-                                                setAnimationDirection("down");
-                                            }
-                                            setSelectedIndex(place.id);
+                                            onClick(place.id);
                                         }
                                     }}
+                                    onAnimationComplete={onAnimationComplete}
                                 />)
                             })
                             : null

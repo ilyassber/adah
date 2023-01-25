8fabc2723331c846d0266c2dbe8dce1701b368a5
ilyassber <ilyass.berchida@gmail.com>
2023-01-25 22:34:23 +0100

    update phone and mail components to include resume

--- ui/cards/contact/PhoneAndMail.tsx
+++ ui/cards/contact/PhoneAndMail.tsx
@@ -10,16 +10,22 @@ const PhoneAndMail: React.FC<PhoneAndMailProps> = (props) => {
     let content = (
         <div className={props.className}>
             <div className="flex flex-row items-center justify-center">
-                <div className="">
-                    <Link className="flex flex-row items-center justify-center mx-6" href="" target="_blank">
+                <div className="mx-6" role="button">
+                    <Link className="flex flex-row items-center justify-center" href="" target="_blank">
                         <Icon className="" src="/icons/phone.svg" alt="" dim="18" />
                         <p className="font-medium text-sm text-[#9197A0] ml-2">+212 628 666599</p>
                     </Link>
                 </div>
-                <div className="">
-                    <Link className="flex flex-row items-center justify-center mx-6" href="" target="_blank">
+                <div className="mx-6" role="button">
+                    <Link className="flex flex-row items-center justify-center" href="" target="_blank">
                         <Icon className="" src="/icons/mail.svg" alt="" dim="18" />
                         <p className="font-medium text-sm text-[#9197A0] ml-2">ilyass.berchida@gmail.com</p>
+                    </Link>
+                </div>
+                <div className="mx-6" role="button">
+                    <Link className="flex flex-row items-center justify-center" href="/files/BERCHIDA-ILYASS-CV.pdf" target="_blank">
+                        <Icon className="" name="DownloadIcon" color="#9197A0" alt="" dim="18" />
+                        <p className="font-medium text-sm text-[#9197A0] ml-2">Download my resume</p>
                     </Link>
                 </div>
             </div>

28e858727b2fd00ae08d35f3edc39e87749d3c13
ilyassber <ilyass.berchida@gmail.com>
2023-01-27 00:22:49 +0100

    update experience component

--- components/other/Experience.tsx
+++ components/other/Experience.tsx
@@ -15,10 +15,10 @@ type ExperienceType = {
 const Experience: React.FC<ExperienceType> = (props) => {
 
     let content = (<div className={props.className}>
-        <div className="flex flex-col px-6 py-4 border border-[#D19E18] rounded-md">
-            <p className="font-bold text-lg text-[#B1B7C0]">{props.position}</p>
+        <div className="flex flex-col px-8 py-6 border-[0.5px] border-[#D19E18BB] hover:bg-[#D19E1822] rounded-md">
+            <p className="font-bold text-base text-[#B1B7C0]">{props.position}</p>
             <div className="flex flex-row mt-1 mb-4">
-                <p className="grow font-medium text-base text-[#D19E18]">{props.company}</p>
+                <p className="grow font-medium text-sm text-[#D19E18]">{props.company}</p>
                 <div className="flex flex-row justify-center items-center mr-6">
                     <Icon className="flex justify-center items-center mr-2" name="CalendarMonthIcon" color="#9197A0" alt="" dim="18" />
                     <p className="inline-block align-middle text-sm text-[#9197A0]">{props.startDate + " - " + props.endDate}</p>

01e3369a76a681b87c52a4fb96c2a397cff2c474
ilyassber <ilyass.berchida@gmail.com>
2023-02-01 23:54:33 +0100

    improve ui for project component

--- components/other/Project.tsx
+++ components/other/Project.tsx
@@ -15,18 +15,17 @@ type ProjectType = {
 const Project: React.FC<ProjectType> = (props) => {
 
     let content = (<div className={props.className}>
-        <div className="h-full flex flex-row">
-            <div className="h-full w-[2px] bg-[#9197A0bb] hidden"></div>
-            <div className="grow flex flex-col px-8 py-6 bg-[#1B1B1B00] shadow-xl rounded-md transition duration-200 ease-in-out transform hover:scale-[1.02] hover:bg-[#1B1B1B66] border-l border-[#D19E18]">
+        <div className="h-full flex flex-col bg-[#1B1B1B00] shadow-xl rounded-md transition duration-200 ease-in-out transform hover:scale-[1.02] hover:bg-[#1B1B1B66] border-l border-[#D19E18BB]">
+            <div className="flex flex-col px-8 py-6">
                 <div className="w-full flex flex-row items-center mb-4">
                     <p className="grow font-bold text-base text-[#B1B7C0] inline-block align-middle">{props.name}</p>
                     {props.projectLink ? (
-                        <Link href={props.projectLink}>
+                        <Link href={props.projectLink} target="_blank">
                             <Icon className="flex justify-center items-center" name="GitHubIcon" color="#9197A0" hoverColor="#D19E18" alt="" dim="25" role='button' />
                         </Link>
                     ) : null}
                     {props.webLink ? (
-                        <Link href={props.webLink}>
+                        <Link href={props.webLink} target="_blank">
                             <Icon className="flex justify-center items-center ml-4" name="PublicIcon" color="#9197A0" hoverColor="#D19E18" alt="" dim="25" role='button' />
                         </Link>
                     ) : null}
@@ -46,8 +45,8 @@ const Project: React.FC<ProjectType> = (props) => {
                 </div>
                 <div className="flex flex-wrap">
                     {props.technologies.map((tech: string, index: number) => {
-                        return (<div key={index} className="flex items-center justify-center bg-[#A1A7B0BB] shadow rounded mr-2 mb-2">
-                            <p key={index} className="inline-block align-middle font-bold text-sm text-[#182B2B] mx-2 my-1">
+                        return (<div key={index} className="flex items-center justify-center bg-[#485B5B] shadow rounded mr-2 mb-2">
+                            <p key={index} className="inline-block align-middle font-bold text-sm text-[#102323] mx-2 my-1">
                                 <span className="whitespace-nowrap"></span>{tech}
                             </p>
                         </div>);

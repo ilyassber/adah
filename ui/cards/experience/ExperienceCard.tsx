dc9c297fec54407ba5d7017a13d626006b344e6f
ilyassber <ilyass.berchida@gmail.com>
2023-02-04 23:17:53 +0100

    update responsiveness style for experience card

--- ui/cards/experience/ExperienceCard.tsx
+++ ui/cards/experience/ExperienceCard.tsx
@@ -24,81 +24,83 @@ const ExperienceCard: React.FC<ExperienceCardProps> = (props) => {
     let content = (
         <div className={props.className}>
             <motion.div
-                className="relative flex h-screen overflow-auto p-24"
+                className="h-screen flex items-center justify-center"
             >
-                <div className="w-full relative flex flex-col justify-center border-l-2 border-dashed border-[#9197A000]">
-                    <Icon className="hidden absolute -left-4 -top-4 flex justify-center items-center mr-2" name="ExpandLessIcon" color="#9197A0DD" alt="" dim="30" />
-                    <motion.div
-                        className="mb-8"
-                        animate={animation}
-                        transition={{
-                            duration: 0.3,
-                            ease: "linear",
-                        }}
-                    >
-                        <UltraExp
+                <div className="relative w-full max-h-screen overflow-auto">
+                    <div className="w-full relative flex flex-col justify-center border-l-2 border-dashed border-[#9197A000] p-24">
+                        <Icon className="hidden absolute -left-4 -top-4 flex justify-center items-center mr-2" name="ExpandLessIcon" color="#9197A0DD" alt="" dim="30" />
+                        <motion.div
+                            className="mb-8"
+                            animate={animation}
+                            transition={{
+                                duration: 0.3,
+                                ease: "linear",
+                            }}
+                        >
+                            <UltraExp
+                                className=""
+                                position="Software developer"
+                                company="UM6P - AGE"
+                                startDate="Jul 2021"
+                                endDate="Present"
+                                location="Bin Jareer, Morocco"
+                                description="AgriEdge is a business unit at UM6P that operates in the agriculture field and provides services to farmers based on data."
+                                tasks={[
+                                    "Creating a cartographic web application for yield prediction based on data provided by users and satellite plus harvest management",
+                                    "Creating a cartographic web application to visualize different data layers using a tiling system",
+                                    "Developing backend services logic and managing databases",
+                                    "Developing REST APIs and GraphQL APIs",
+                                    "Ensuring access permissions and security"
+                                ]}
+                            />
+                        </motion.div>
+                        <motion.div
+                            className="mb-8"
+                            animate={animation}
+                            transition={{
+                                duration: 0.6,
+                                ease: "linear",
+                            }}
+                        >
+                            <UltraExp
+                                className=""
+                                position="Back-end Developer - Internship"
+                                company="ECOM ANALYTIC"
+                                startDate="Nov 2020"
+                                endDate="Apr 2021"
+                                location="Khouribga, Morocco"
+                                description="1337 Startup provides ecommerce solutions."
+                                tasks={[
+                                    "Creating a web crawler for e-commerce websites and a data scraper to collect data about products in different categories",
+                                    "Creating a database to store collected data",
+                                    "Developing a background process that keep track on scraped product and categories, and manage scraping schedule for each item",
+                                    "Developing a separated crawling and scraping as decentralized services on many servers to facilitate scaling",
+                                    "Creating a REST API to link between all services"
+                                ]}
+                            />
+                        </motion.div>
+                        <motion.div
                             className=""
-                            position="Software developer"
-                            company="UM6P - AGE"
-                            startDate="Jul 2021"
-                            endDate="Present"
-                            location="Bin Jareer, Morocco"
-                            description="AgriEdge is a business unit at UM6P that operates in the agriculture field and provides services to farmers based on data."
-                            tasks={[
-                                "Creating a cartographic web application for yield prediction based on data provided by users and satellite plus harvest management",
-                                "Creating a cartographic web application to visualize different data layers using a tiling system",
-                                "Developing backend services logic and managing databases",
-                                "Developing REST APIs and GraphQL APIs",
-                                "Ensuring access permissions and security"
-                            ]}
-                        />
-                    </motion.div>
-                    <motion.div
-                        className="mb-8"
-                        animate={animation}
-                        transition={{
-                            duration: 0.6,
-                            ease: "linear",
-                        }}
-                    >
-                        <UltraExp
-                            className=""
-                            position="Back-end Developer - Internship"
-                            company="ECOM ANALYTIC"
-                            startDate="Nov 2020"
-                            endDate="Apr 2021"
-                            location="Khouribga, Morocco"
-                            description="1337 Startup provides ecommerce solutions."
-                            tasks={[
-                                "Creating a web crawler for e-commerce websites and a data scraper to collect data about products in different categories",
-                                "Creating a database to store collected data",
-                                "Developing a background process that keep track on scraped product and categories, and manage scraping schedule for each item",
-                                "Developing a separated crawling and scraping as decentralized services on many servers to facilitate scaling",
-                                "Creating a REST API to link between all services"
-                            ]}
-                        />
-                    </motion.div>
-                    <motion.div
-                        className=""
-                        animate={animation}
-                        transition={{
-                            duration: 0.9,
-                            ease: "linear",
-                        }}
-                    >
-                        <UltraExp
-                            className=""
-                            position="Flutter Developer"
-                            company="ETIL"
-                            startDate="Jul 2019"
-                            endDate="Jan 2020"
-                            location="Essaouira, Morocco"
-                            description="Essaouira Territorial Innovation Lab (ETIL), an open innovation space serving the territorial developement of the Essaouira province."
-                            tasks={[
-                                "Creating a digital guide as a mobile application to provide city visitors with the information about the sities they visit and their monuments, history, and culture",
-                            ]}
-                        />
-                    </motion.div>
+                            animate={animation}
+                            transition={{
+                                duration: 0.9,
+                                ease: "linear",
+                            }}
+                        >
+                            <UltraExp
+                                className=""
+                                position="Flutter Developer"
+                                company="ETIL"
+                                startDate="Jul 2019"
+                                endDate="Jan 2020"
+                                location="Essaouira, Morocco"
+                                description="Essaouira Territorial Innovation Lab (ETIL), an open innovation space serving the territorial developement of the Essaouira province."
+                                tasks={[
+                                    "Creating a digital guide as a mobile application to provide city visitors with the information about the sities they visit and their monuments, history, and culture",
+                                ]}
+                            />
+                        </motion.div>
+                    </div>
                 </div>
             </motion.div >
         </div >

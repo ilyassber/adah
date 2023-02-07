ece8edd66ebc4b4d62170e35356884e25c8c4449
ilyassber <ilyass.berchida@gmail.com>
2023-02-07 23:43:10 +0100

    update phone and mail links

--- ui/cards/PhoneAndMail.tsx
+++ ui/cards/PhoneAndMail.tsx
@@ -11,13 +11,13 @@ const PhoneAndMail: React.FC<PhoneAndMailProps> = (props) => {
         <div className={props.className}>
             <div className="flex flex-row items-center justify-center">
                 <div className="mx-6" role="button">
-                    <Link className="flex flex-row items-center justify-center" href="" target="_blank">
+                    <Link className="flex flex-row items-center justify-center" href="tel:+212-628-666599" target="_blank">
                         <Icon className="" src="/icons/phone.svg" alt="" dim="18" />
                         <p className="font-medium text-sm text-[#9197A0] ml-2">+212 628 666599</p>
                     </Link>
                 </div>
                 <div className="mx-6" role="button">
-                    <Link className="flex flex-row items-center justify-center" href="" target="_blank">
+                    <Link className="flex flex-row items-center justify-center" href="mailto:ilyass.berchida@gmail.com" target="_blank">
                         <Icon className="" src="/icons/mail.svg" alt="" dim="18" />
                         <p className="font-medium text-sm text-[#9197A0] ml-2">ilyass.berchida@gmail.com</p>
                     </Link>

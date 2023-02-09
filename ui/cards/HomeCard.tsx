f47dcb8ad303a359ae3eb13cc4553b7759fc71ae
ilyassber <ilyass.berchida@gmail.com>
2023-02-09 23:47:24 +0100

    fix home card responsiveness

--- ui/cards/HomeCard.tsx
+++ ui/cards/HomeCard.tsx
@@ -10,7 +10,7 @@ const HomeCard: React.FC<HomeCardProps> = (props) => {
     let content = (
         <div className={props.className}>
             <motion.div
-                className="p-24"
+                className="p-8 sm:p-12 md:p-20 lg:p-24"
                 animate={{
                     y: [-20, 0],
                     opacity: ["0%", "100%"],
@@ -20,17 +20,17 @@ const HomeCard: React.FC<HomeCardProps> = (props) => {
                     ease: "easeOut",
                 }}
             >
-                <p className="font-semibold text-xl text-yano-500 mb-8">
+                <p className="font-semibold text-base sm:text-lg md:text-xl text-yano-500 mb-2 sm:mb-4 md:mb-6 lg:mb-8">
                     Hi, I'm
                 </p>
-                <p className="font-montserrat font-bold text-5xl text-[#E2E8F0] mb-8">
+                <p className="font-montserrat font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-[#E2E8F0] mb-2 sm:mb-4 md:mb-6 lg:mb-8">
                     Ilyass BERCHIDA
                 </p>
-                <p className="font-bold text-5xl text-[#9197A0] leading-normal mb-8">
+                <p className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-[#9197A0] leading-normal mb-2 sm:mb-4 md:mb-6 lg:mb-8">
                     I build things & ensure<br />functionality.
                 </p>
-                <p className="font-normal text-lg text-[#9197A0] leading-relaxed">
-                    I'm a <span className="font-semibold text-yano-500">Software developer</span> specialized in creating digital<br />experiences mainly for the web. Currently focusing on building<br />digital tools that enhance agriculture at <span className="font-semibold text-yano-500">AgriEdge</span>.
+                <p className="max-w-lg font-normal text-xs sm:text-sm md:text-base text-[#9197A0] leading-relaxed">
+                    I'm a <span className="font-semibold text-yano-500">Software developer</span> specialized in creating digital experiences mainly for the web. Currently focusing on building digital tools that enhance agriculture at <span className="font-semibold text-yano-500">AgriEdge</span>.
                 </p>
             </motion.div>
         </div>

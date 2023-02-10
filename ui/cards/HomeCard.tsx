c82e3ccd61e7e19439f15a1b5adc91df0f1d58ed
ilyassber <ilyass.berchida@gmail.com>
2023-02-10 23:23:47 +0100

    fix home sections alignment

--- ui/cards/HomeCard.tsx
+++ ui/cards/HomeCard.tsx
@@ -20,13 +20,13 @@ const HomeCard: React.FC<HomeCardProps> = (props) => {
                     ease: "easeOut",
                 }}
             >
-                <p className="font-semibold text-base sm:text-lg md:text-xl text-yano-500 mb-2 sm:mb-4 md:mb-6 lg:mb-8">
+                <p className="font-semibold text-base sm:text-lg md:text-xl text-yano-500 mb-4 sm:mb-4 md:mb-6 lg:mb-8">
                     Hi, I'm
                 </p>
-                <p className="font-montserrat font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-[#E2E8F0] mb-2 sm:mb-4 md:mb-6 lg:mb-8">
+                <p className="font-montserrat font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-[#E2E8F0] mb-4 sm:mb-4 md:mb-6 lg:mb-8">
                     Ilyass BERCHIDA
                 </p>
-                <p className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-[#9197A0] leading-normal mb-2 sm:mb-4 md:mb-6 lg:mb-8">
+                <p className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-[#9197A0] leading-normal mb-4 sm:mb-4 md:mb-6 lg:mb-8">
                     I build things & ensure<br />functionality.
                 </p>
                 <p className="max-w-lg font-normal text-xs sm:text-sm md:text-base text-[#9197A0] leading-relaxed">

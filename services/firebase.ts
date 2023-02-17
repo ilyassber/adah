bc5090ff8cdc47b83df46b0b9690f78e0b68503f
ilyassber <ilyass.berchida@gmail.com>
2023-02-17 23:32:18 +0100

    add firebase configs and init firebase

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,1 +1,17 @@
-export const sendMessage = (message: String) => {};
+import { initializeApp, FirebaseApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+    };
+
+export const initFirebase = (): FirebaseApp => {
+
+    // Initialize Firebase
+    return initializeApp(firebaseConfig);
+};

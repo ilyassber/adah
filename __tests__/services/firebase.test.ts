3fa6ec6229c6eaa57a7a404304c86a191f1d1720
ilyassber <ilyass.berchida@gmail.com>
2023-02-21 01:58:24 +0100

    set a unit test for init firebase

--- __tests__/services/firebase.test.ts
+++ __tests__/services/firebase.test.ts
@@ -1,4 +1,30 @@
-import '@testing-library/jest-dom'
+import { initFirebase, initAnalytics, sendMessage } from "@/services/firebase";
+import { Message } from '../../types.d';
+import {describe, expect, test} from '@jest/globals';
+
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+};
+
+const message: Message = {
+  message: "",
+  sendDateTime: "1900-02-02T00:00:00"
+};
+
+describe("Firebase functions", () => {
+  describe("initFirebase", () => {
+    test("should initialize Firebase app", () => {
+      const app = initFirebase(firebaseConfig);
+      expect(app).toBeDefined();
+    });
+  });
+});
 
 
 
--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,50 @@
-const nextJest = require("next/jest");
-
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./",
-});
-
-// Add any custom config to be passed to Jest
-const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+module.exports = {
+  collectCoverage: true,
+  // on node 14.x coverage provider v8 offers good speed and more or less good report
+  coverageProvider: "v8",
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+    "!<rootDir>/out/**",
+    "!<rootDir>/.next/**",
+    "!<rootDir>/*.config.js",
+    "!<rootDir>/coverage/**",
+  ],
   moduleNameMapper: {
-    // Handle module aliases (this will be automatically configured for you soon)
+    // Handle CSS imports (with CSS modules)
+    // https://jestjs.io/docs/webpack#mocking-css-modules
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+
+    // Handle CSS imports (without CSS modules)
+    "^.+\\.(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+
+    // Handle image imports
+    // https://jestjs.io/docs/webpack#handling-static-assets
+    "^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i": `<rootDir>/__mocks__/fileMock.js`,
+
+    // Handle module aliases
     "^@/components/(.*)$": "<rootDir>/components/$1",
-    "^@/pages/(.*)$": "<rootDir>/pages/$1",
     "^@/services/(.*)$": "<rootDir>/services/$1",
   },
-  testEnvironment: "jest-environment-jsdom",
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  testEnvironment: "node", //"jsdom"
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset
+    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    "^.+\\.(js|jsx)$": ["babel-jest", { presets: ["next/babel"] }],
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "^.+\\.module\\.(css|sass|scss)$",
+  ],
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig);
+const path = require("path");
 
-// const path = require("path");
+const dotenv = require("dotenv");
 
-// const dotenv = require("dotenv");
-
-// dotenv.config({ path: path.resolve(process.cwd(), ".env.local") });
+dotenv.config({ path: path.resolve(process.cwd(), ".env.local") });
